8c5a23709c0daedf0437b3e48cb95b45e8431ea1
Bruno Almeida <brunoalmeida195@gmail.com>
2018-07-18 20:53:03 -0300

    Add new pratical example FizzBuzz

---
+++ app/fizzbuzz/main.js
@@ -0,0 +1,8 @@
+const FizzBuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
+  return number;
+}
+
+export default FizzBuzz;
---
+++ app/fizzbuzz/test.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import FizzBuzz from './main';
+
+describe('FizzBuzz', () => {
+
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+  });
+
+  it('should return `Buzz` when miltiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+  });
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  });
+
+  it('should return number when its not multiple', () => {
+    expect(FizzBuzz(7)).to.be.equal(7);
+  });
+});
