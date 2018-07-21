82f4cfb608eb7116c2b3446bcdb476e549f4653a
Bruno Almeida <brunoalmeida195@gmail.com>
2018-07-21 15:08:50 -0300

    Downgrade packages

--- app/spotify-wrapper/test.spec.js
+++ app/spotify-wrapper/test.spec.js
@@ -1,9 +1,11 @@
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 import { search, searchArtists, searchAlbums, searchTracks, searchPlaylists } from './main'
 
 chai.use(sinonChai);
+sinonStubPromise(sinon);
 global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', () => {
