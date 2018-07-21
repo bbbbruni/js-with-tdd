98b49848ff211c1e609e288a41c8ea4eab0caa56
Bruno Almeida <brunoalmeida195@gmail.com>
2018-07-20 23:54:52 -0300

    Progress of tests practice

--- app/spotify-wrapper/main.js
+++ app/spotify-wrapper/main.js
@@ -1,9 +1,22 @@
-const search = (query, type) =>
-  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
+const search = (query, type) => {
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(res => res.json())
+}
 
-const searchArtists = () => {};
-const searchAlbums = () => {};
-const searchTracks = () => {};
-const searchPlaylists = () => {};
+const searchArtists = (query) => {
+  search(query, 'artist');
+};
+
+const searchAlbums = (query) => {
+  search(query, 'album');
+};
+
+const searchTracks = (query) => {
+  search(query, 'tracks');
+};
+
+const searchPlaylists = (query) => {
+  search(query, 'playlist');
+};
 
 export { search, searchArtists, searchAlbums, searchTracks, searchPlaylists };
--- app/spotify-wrapper/test.spec.js
+++ app/spotify-wrapper/test.spec.js
@@ -1,14 +1,27 @@
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import sinonStubPromise from 'sinon-stub-promise';
 import { search, searchArtists, searchAlbums, searchTracks, searchPlaylists } from './main'
 
 chai.use(sinonChai);
-sinonStubPromise(sinon);
 global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', () => {
+  let fetchedStub;
+  let promise;
+
+  // Hooks
+  beforeEach(() => {
+    // Use and handle fetch to work with node-fetch
+    fetchedStub = sinon.stub(global, 'fetch');
+    // Sinon handle promises return
+    promise = fetchedStub.returnsPromise();
+  });
+
+  afterEach(() => {
+    // Restores the XHR constructor(fetchedStub) to use on another test block
+    fetchedStub.restore();
+  });
 
   describe('Smoke tests', () => {
     it('should exist search method', () => {
@@ -34,38 +47,81 @@ describe('Spotify Wrapper', () => {
   });
 
   describe('Generic Search', () => {
-    let fetchedStub;
-
-    // Hooks
-    beforeEach(() => {
-      fetchedStub = sinon.stub(global, 'fetch');
-    });
-
-    afterEach(() => {
-      // Restores the XHR constructor(fetchedStub) to use on another test block
-      fetchedStub.restore();
-    });
-
     it('should call fetch function', () => {
       const artists = search();
       expect(fetchedStub).to.have.been.calledOnce;
     });
 
-    describe('Passing on type', () => {
-      it('should receive correct url to fetch spotify API', () => {
+    it('should receive correct url to fetch spotify API', () => {
+      describe('passing on type', () => {
         const artists = search('drake', 'artist');
         expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=drake&type=artist');
 
         const albums = search('drake', 'album');
         expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=drake&type=album');
       });
+
+      describe('passing two or more types', () => {
+        const artistAndAlbums = search('drake', ['artist', 'album']);
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=drake&type=artist,album');
+      });
     });
 
-    describe('Passing two or more types', () => {
-      it('should receive correct url to fetch spotify API', () => {
-        const artistAndAlbums = search('drake', ['album', 'artist']);
-        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=drake&type=album,artist');
-      });
+    // TODO: Refator this code with the update of the teacher => https://bit.ly/2LAMZcX
+    it.skip('should return JSON data from the promise', () => {
+      promise.resolves({ body: 'json' });
+      const artist = search('paramore', 'album');
+
+      expect(artist.resolveValue).to.be.eql({body: 'json'});
+    });
+
+  });
+
+  describe('Search Artist', () => {
+    it('should call fetch function', () => {
+      const artist = searchArtists('drake');
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should fetch ARTIST with correct URL from Spotify API', () => {
+      const artist = searchArtists('drake');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=drake&type=artist');
+    });
+  });
+
+  describe('Search Albums', () => {
+    it('should call fetch function', () => {
+      const album = searchAlbums('paramore');
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should fetch ALBUM with correct URL from Spotify API', () => {
+      const album = searchAlbums('paramore');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=paramore&type=album');
+    });
+  });
+
+  describe('Search Tracks', () => {
+    it('should call fetch function', () => {
+      const tracks = searchTracks('blink-182');
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should fetch TRACKS with correct URL from Spotify API', () => {
+      const tracks = searchTracks('blink-182');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=blink-182&type=tracks');
+    });
+  });
+
+  describe('Search Playlist', () => {
+    it('should call fetch function', () => {
+      const playlists = searchPlaylists('lorde');
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should fetch PLAYLISTS with correct URL from Spotify API', () => {
+      const playlists = searchPlaylists('lorde');
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=lorde&type=playlist');
     });
   });
 });
